d184c16f4dff702a8f59fc5b22bdc7c3f9c582d5
MatthewReid854 <m.reid854@gmail.com>
2019-08-30 16:19:13 -0400

    version update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="reliability",
-    version="0.2.1",
+    version="0.2.2",
     description="Reliability Engineering toolkit for Python",
     author="Matthew Reid",
     author_email="m.reid854@gmail.com",
