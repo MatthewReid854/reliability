894c2a9996b608614ce1f0cc48131353873ebee4
MatthewReid854 <m.reid854@gmail.com>
2020-08-06 11:44:24 +1000

    updating tests

--- tests/test_trial.py
+++ tests/test_trial.py
@@ -1,6 +1,3 @@
-#this is my first go at a test
-#trying to get this working with travis-ci
-
 import pytest
 from reliability.Distributions import Normal_Distribution
 
@@ -10,9 +7,10 @@ def test_Normal_Distribution():
     assert dist.standard_deviation == 2
     assert dist.variance == 4
 
-
-def test_Normal_Distribution_willfail():
+def test_Normal_Distribution2():
     dist = Normal_Distribution(mu=5, sigma=2)
-    assert dist.mean == 0
-    assert dist.standard_deviation == 0
-    assert dist.variance == 0
+    assert dist.mean == 5
+    assert dist.standard_deviation == 2
+    assert dist.variance == 4
+
+#need to add more tests.
